aa4ce9d87b3fe57fa28b006582684115fda61979
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-03-13 00:11:59 -0700

    apache, mysql, mysql-x86 are building together now
    This is a revert back to 73aa3b8

--- config/software/apache.rb
+++ config/software/apache.rb
@@ -1,6 +1,7 @@
 name "apache"
+default_version "2.4.25"
 
-source url: "http://installer-bin.streambox.com.s3.amazonaws.com/httpd-2.4.25-win32-VC14.zip",
+source url: "http://installer-bin.streambox.com.s3.amazonaws.com/httpd-#{version}-win32-VC14.zip",
        md5: '259c62962f45b2c1d7fd0ea2b3293760'
 
 build do
--- config/software/oo1.rb
+++
@@ -1,5 +0,0 @@
-name "oo1"
-
-dependency "apache"
-dependency "mysql-x86"
-dependency "mysql"
