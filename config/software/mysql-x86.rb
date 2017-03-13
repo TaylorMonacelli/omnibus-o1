4b6c334da95feb5ee02a7e4b0bbac7e8cd8c5122
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-03-12 22:49:35 -0700

    Remove #{version} from url and default_verison listing

--- config/software/mysql-x86.rb
+++ config/software/mysql-x86.rb
@@ -1,7 +1,6 @@
 name "mysql-x86"
-default_version "5.6.24"
 
-source url: "https://downloads.mysql.com/archives/get/file/mysql-#{version}-win32.zip",
+source url: "https://downloads.mysql.com/archives/get/file/mysql-5.6.24-win32.zip",
        md5: '2b7fd9f9b236e1ec1653d3575fc1cd46'
 
 build do
