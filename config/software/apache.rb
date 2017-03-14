f62cb98afcaee0196e93e284b052b100360c095a
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-03-13 20:42:23 -0700

    Factor url out

--- config/software/apache.rb
+++ config/software/apache.rb
@@ -6,14 +6,14 @@ when "2.4.25"
   vc = "VC14"
 end
 
+source url: "http://installer-bin.streambox.com.s3.amazonaws.com/httpd-#{version}-#{platform}-#{vc}.zip"
+
 if windows_arch_i386? 
   platform = "win32" 
-  source url: "http://installer-bin.streambox.com.s3.amazonaws.com/httpd-#{version}-#{platform}-#{vc}.zip",
-         md5: "259c62962f45b2c1d7fd0ea2b3293760"
+  source md5: "259c62962f45b2c1d7fd0ea2b3293760"
 else
   platform = "win64"
-  source url: "http://installer-bin.streambox.com.s3.amazonaws.com/httpd-#{version}-#{platform}-#{vc}.zip",
-         md5: "eb6642b253fcf04d288c0f518f131acd"
+  source md5: "eb6642b253fcf04d288c0f518f131acd"
 end
 
 build do
