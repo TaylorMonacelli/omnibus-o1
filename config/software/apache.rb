10dc6727b9bc58f37a29c2c5aa6a1ff22f3aa7fd
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-03-13 20:46:12 -0700

    Fix platform unedfined

--- config/software/apache.rb
+++ config/software/apache.rb
@@ -6,8 +6,6 @@ when "2.4.25"
   vc = "VC14"
 end
 
-source url: "http://installer-bin.streambox.com.s3.amazonaws.com/httpd-#{version}-#{platform}-#{vc}.zip"
-
 if windows_arch_i386? 
   platform = "win32" 
   source md5: "259c62962f45b2c1d7fd0ea2b3293760"
@@ -15,6 +13,8 @@ else
   platform = "win64"
   source md5: "eb6642b253fcf04d288c0f518f131acd"
 end
+
+source url: "http://installer-bin.streambox.com.s3.amazonaws.com/httpd-#{version}-#{platform}-#{vc}.zip"
 
 build do
   copy "Apache24", "#{install_dir}/apache"
--- config/software/mysql.rb
+++ config/software/mysql.rb
@@ -1,8 +1,6 @@
 name "mysql"
 default_version "5.6.24"
 
-source url: "https://downloads.mysql.com/archives/get/file/mysql-#{version}-#{platform}.zip"
-
 if windows_arch_i386?
   platform = "win32"
   source md5: "2b7fd9f9b236e1ec1653d3575fc1cd46"
@@ -10,6 +8,8 @@ else
   platform = "winx64"  
   source md5: "8d864c8a90c6d7b2ede48a74ac66dbf3"
 end
+
+source url: "https://downloads.mysql.com/archives/get/file/mysql-#{version}-#{platform}.zip"
 
 build do
   copy "mysql-#{version}-#{platform}", "#{install_dir}/mysql"
