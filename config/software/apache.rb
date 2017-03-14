78b1a3185c3a2afb66cd9ef2b9ff7ce7062db744
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-03-13 22:39:30 -0700

    Rename platform variabl to architecture

--- config/software/apache.rb
+++ config/software/apache.rb
@@ -6,15 +6,15 @@ when "2.4.25"
   vc = "VC14"
 end
 
-if windows_arch_i386? 
-  platform = "win32" 
+if windows_arch_i386?
+  arch_suffix = "win32" 
   source md5: "259c62962f45b2c1d7fd0ea2b3293760"
 else
-  platform = "win64"
+  arch_suffix = "win64"
   source md5: "eb6642b253fcf04d288c0f518f131acd"
 end
 
-source url: "http://installer-bin.streambox.com.s3.amazonaws.com/httpd-#{version}-#{platform}-#{vc}.zip"
+source url: "http://installer-bin.streambox.com.s3.amazonaws.com/httpd-#{version}-#{arch_suffix}-#{vc}.zip"
 
 build do
   copy "Apache24", "#{install_dir}/apache"
--- config/software/mysql.rb
+++ config/software/mysql.rb
@@ -2,15 +2,15 @@ name "mysql"
 default_version "5.6.24"
 
 if windows_arch_i386?
-  platform = "win32"
+  arch_suffix = "win32"
   source md5: "2b7fd9f9b236e1ec1653d3575fc1cd46"
 else
-  platform = "winx64"  
+  arch_suffix = "winx64"
   source md5: "8d864c8a90c6d7b2ede48a74ac66dbf3"
 end
 
-source url: "https://downloads.mysql.com/archives/get/file/mysql-#{version}-#{platform}.zip"
+source url: "https://downloads.mysql.com/archives/get/file/mysql-#{version}-#{arch_suffix}.zip"
 
 build do
-  copy "mysql-#{version}-#{platform}", "#{install_dir}/mysql"
+  copy "mysql-#{version}-#{arch_suffix}", "#{install_dir}/mysql"
 end  
