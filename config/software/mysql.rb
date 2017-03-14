a0e2f98d47f79746a4ed36461708600acdc9ee80
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-03-13 19:49:16 -0700

    Revert "Use single quotes when not interpolating"
    This reverts commit f074687b1bad3bfca5f77dc0bbe7eb7acd1eb59f.

--- config/software/mysql.rb
+++ config/software/mysql.rb
@@ -1,7 +1,7 @@
-name 'mysql'
-default_version '5.6.24'
+name "mysql"
+default_version "5.6.24"
 
-source url: 'https://downloads.mysql.com/archives/get/file/mysql-5.6.24-winx64.zip',
+source url: "https://downloads.mysql.com/archives/get/file/mysql-5.6.24-winx64.zip",
        md5: '8d864c8a90c6d7b2ede48a74ac66dbf3'
 
 build do
